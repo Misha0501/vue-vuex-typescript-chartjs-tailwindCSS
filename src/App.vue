fe477158a0aa3209235266a29b616bfb3c96f70d
misha0501 <misha.galenda@gmail.com>
2024-02-03 20:36:28 +0100

    Init

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import PageHeader from "@/components/PageHeader.vue";
+</script>
+
+<template>
+  <header>
+    <div class="wrapper bg-amber-300">
+      <PageHeader msg="Hello world"/>
+    </div>
+  </header>
+
+  <main>
+    <p>Main part</p>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div>
+    <h1 class="bg-red-500">{{ msg }}</h1>
+  </div>
+</template>
+
+<style scoped></style>
