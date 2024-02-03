1891640932351edbd5dbd8e01378117029eabcf2
misha0501 <misha.galenda@gmail.com>
2024-02-03 20:37:34 +0100

    styling cleanup

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import PageHeader from "@/components/PageHeader.vue";
 
 <template>
   <header>
-    <div class="wrapper bg-amber-300">
+    <div class="bg-amber-300">
       <PageHeader msg="Hello world"/>
     </div>
   </header>
@@ -14,31 +14,4 @@ import PageHeader from "@/components/PageHeader.vue";
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
