1d1cde431a04095ed850f1320243338c3086eb1e
misha0501 <misha.galenda@gmail.com>
2024-02-04 17:53:02 +0100

    updated base url to depend on NODE_ENV

--- vite.config.ts
+++ vite.config.ts
@@ -15,5 +15,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: '/poll/'
+  base: process.env.NODE_ENV === "production" ? "/poll/" : "/",
 })
